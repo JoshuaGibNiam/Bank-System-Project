23003992e65a8fbc8d427936b3794b8e1682429b
Joshua Niam <joshuaniamjy@gmail.com>
2025-02-16 17:47:25 +0800

    Added bank.py file, changed attr of BankAccount class and added new methods into it

--- bank_account.py
+++ bank_account.py
@@ -1,9 +1,22 @@
-"""Manages individual bank accounts"""
-
 class BankAccount:
-    def __init__(self, name: str):
-        self.name = name
-        self.balance = 0
-        self.deposits = 0
-        self.withdrawals = 0
+    """Manages individual bank accounts"""
+    def __init__(self, account_holder: str, account_number: int):
+        self.__account_holder = account_holder
+        self.__account_number = account_number
+        self.__balance = 0
+
+    def get_account_holder(self) -> str:
+        return self.__account_holder
+
+    def get_account_number(self) -> int:
+        return self.__account_number
+
+    def get_balance(self) -> int:
+        return self.__balance
+
+    def deposit(self, amount: int):
+        self.__balance += amount if amount > 0 else 0
+
+    def withdraw(self, amount: int):
+        self.__balance -= amount if amount < 0 and self.__balance > amount else 0
 
