65b3521e8fa97e2a3dfa87b5a333411d4d1d0478
Joshua Niam <joshuaniamjy@gmail.com>
2025-02-16 18:11:06 +0800

    Added __str__ dunder method to BankAccount class

--- bank_account.py
+++ bank_account.py
@@ -4,16 +4,17 @@ class BankAccount:
         self.__account_holder = account_holder
         self.__account_number = account_number
         self.__balance = initial_balance
-
+    def __str__(self):
+        return f"BankAccount(account_holder = {self.__account_holder}, account_number = {self.__account_number}"
     def get_account_holder(self) -> str:
         """Returns the account holder in a string"""
         return self.__account_holder
 
-    def get_account_number(self) -> float:
+    def get_account_number(self) -> int:
         """Returns the account number in a float"""
         return self.__account_number
 
-    def get_balance(self) -> int:
+    def get_balance(self) -> float:
         """Returns the account balance in a float"""
         return self.__balance
 
@@ -24,4 +25,6 @@ class BankAccount:
     def withdraw(self, amount: int):
         """Withdraws an amount from the account, checks if the withdrawal is possible"""
         self.__balance -= amount if amount < 0 and self.__balance > amount else 0
+
+
 
