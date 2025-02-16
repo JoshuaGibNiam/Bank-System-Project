9c6bce9bf3930a8fe7e44b87653e9c95afe0fab3
Joshua Niam <joshuaniamjy@gmail.com>
2025-02-16 21:55:42 +0800

    Added bank.py file for Bank class and created multiple methods

--- bank.py
+++ bank.py
@@ -1,1 +1,53 @@
 from bank_account import BankAccount
+import random
+class Bank:
+    """
+    The Bank class represents a financial institution that manages multiple bank accounts. It serves
+    as a central hub for handling account creation, deletion, retrieval, and transfers between
+    accounts.
+    """
+    def __init__(self):
+        self.__accounts = {}
+
+    def add_account(self):
+        holder = input("Enter account holder name: ").strip()
+        while not holder.isalpha():
+            print("Account holder name must only contain alphabets!")
+            holder = input("Enter account holder name: ").strip()
+        number = self.generate_randint()
+        self.__accounts[number] = BankAccount(holder, str(number), 0)
+        print(f"Bank account of {holder} successfully created.")
+        return True
+
+    def retrieve_account(self, num):
+        return self.__accounts[num]
+
+    def delete_account(self, num):
+        del self.__accounts[num]
+        print(f"Bank account {num} deleted.")
+        return True
+
+    def generate_randint(self):
+        number = random.randint(1, 100000)
+        while number in self.__accounts:
+            number = random.randint(1, 100000)
+        return number
+
+    def list_accounts(self):
+        print("Active bank accounts:")
+        for key, instance in self.__accounts.items():
+            print(f"{key}: {instance.get_account_holder()}")
+        print("End of list. \n")
+        return True
+
+    def transfer_amount(self, account1, account2, amount):
+        """Transfers amount from account 1 to account 2 (pass holder name as arg)"""
+        for values in self.__accounts.values():
+            if values.get_account_holder() == account1:
+                account1 = values    #reassign account1 to BankAccount instance
+            if values.get_account_holder() == account2:
+                account2 = values
+        if account1.withdraw(amount):
+            account2.deposit(amount)
+        else:
+            print("Transfer failed.")
--- bank_account.py
+++ bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
     """Manages individual bank accounts"""
-    def __init__(self, account_holder: str, account_number: int, initial_balance: float):
+    def __init__(self, account_holder: str, account_number: str, initial_balance: float):
         self.__account_holder = account_holder
         self.__account_number = account_number
         self.__balance = initial_balance
@@ -19,12 +19,36 @@ class BankAccount:
         return self.__balance
 
     def deposit(self, amount: int):
-        """Deposits an amount into the account, checks if the deposit is positive"""
-        self.__balance += amount if amount > 0 else 0
-
+        """Deposits an amount into the account, checks if the deposit is positive
+        Return True if deposit was successful, False if not"""
+        if amount >= 0:
+            self.__balance += amount
+            print(f"Successfully deposited {amount} into the account.")
+            return True
+        else:
+            print("Invalid amount to be deposited.")
+            return False
     def withdraw(self, amount: int):
-        """Withdraws an amount from the account, checks if the withdrawal is possible"""
-        self.__balance -= amount if amount < 0 and self.__balance > amount else 0
+        """Withdraws an amount from the account, checks if the withdrawal is possible
+        Returns True if withdrawal was successful, False if not"""
+        if  amount > 0 and self.__balance >= amount:
+            self.__balance -= amount
+            print(f"Successfully withdrawn {amount} from the account.")
+            return True
+        else:
+            print("Insufficient funds / Invalid amount")
+            return False
 
 
 
+
+if __name__ == "__main__":
+    print("TEST1: Create a new account.")
+    account = BankAccount("John", "1234", 10000)
+    print(account)
+    account.deposit(100)
+    account.get_balance()
+    account.withdraw(20000)
+    account.get_balance()
+    account.deposit(-22)
+    account.get_account_number()
